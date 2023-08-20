257c2bb14f5a743fad209ba02f269d585c8c7d5d
Alien <lennhypolite@macbook-pro.myfiosgateway.com>
2023-08-19 23:10:07 -0400

    Add iframe for youtube videos.

--- frontend/src/components/IllustrationsPage.js
+++ frontend/src/components/IllustrationsPage.js
@@ -1,8 +1,11 @@
 export default function IllustrationsPage() {
+    const bucketFolder = "http://polyverge-images.s3-website-us-east-1.amazonaws.com/illustrations/"
     return (
         <>
           <section className="">
           <h1> Illustrations Page</h1>
+          {/* Need a loop here that returns images from S3 bucket - Need to do this in the backend or will have to hard code it here */}
+          <img src=""/>
           </section>
         </>
       )
