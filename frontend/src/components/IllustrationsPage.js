1a64b1cc09db1a3be32d09139eb4d521ba8973e1
Alien <lennhypolite@macbook-pro.myfiosgateway.com>
2023-08-20 18:18:16 -0400

    Add illustration component and image links. Need to change these links into dynamic links.

--- frontend/src/components/IllustrationsPage.js
+++ frontend/src/components/IllustrationsPage.js
@@ -5,7 +5,11 @@ export default function IllustrationsPage() {
           <section className="">
           <h1> Illustrations Page</h1>
           {/* Need a loop here that returns images from S3 bucket - Need to do this in the backend or will have to hard code it here */}
-          <img src=""/>
+          <img src="https://polyverge-images.s3.amazonaws.com/thumbs/armada-thumb.jpg"/>
+          <img src="https://polyverge-images.s3.amazonaws.com/thumbs/dark-side-thumb.jpg"/>
+          <img src="https://polyverge-images.s3.amazonaws.com/thumbs/miko-thumb.jpg"/>
+          <img src="https://polyverge-images.s3.amazonaws.com/thumbs/paralysis-thumb.jpg"/>
+
           </section>
         </>
       )
