b86be12052cb2dc84a7e8d04d2ea47aa0043528f
Alien <lennhypolite@aliens-mbp.myfiosgateway.com>
2023-08-22 23:50:54 -0400

    Got post request to work with prevent default by using formData function in the handleSubmit function to structure the data in the request to mimic form field key pair values.

--- frontend/src/components/MailchimpForm.js
+++ frontend/src/components/MailchimpForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from 'axios';
 
 export default function MailchimpForm() {
   const url = process.env.REACT_APP_MAILCHIMP_POST_URL
@@ -14,15 +15,89 @@ export default function MailchimpForm() {
   //   setMyCar(event.target.value)
   // }
 
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    console.log(email)
+    console.log(firstName)
+    console.log(lastName)
+
+    const data = {
+      'u': process.env.REACT_APP_MAILCHIMP_U,
+      'id': process.env.REACT_APP_MAILCHIMP_ID,
+      'MERGE0' : email,
+      'MERGE1' :firstName,
+      'MERGE2' : lastName,
+    };
+    var formData = new FormData();
+
+    formData.append('u', process.env.REACT_APP_MAILCHIMP_U)
+    formData.append('id', process.env.REACT_APP_MAILCHIMP_ID,)
+    formData.append('MERGE0', email)
+    formData.append( 'MERGE1', firstName)
+    formData.append( 'MERGE2', lastName)
+    console.log(formData)
+    // axios
+    //   .post(postURL, 
+    //     data, {
+    //     headers: {
+    //       Accept: "application/json",
+    //       "Content-Type": "application/json;charset=UTF-8",
+    //     },
+    //   })
+    //   .then(({data}) => {
+    //     console.log(data);
+    // });
+
+    // axios({
+    //   method: 'post',
+    //   mode: 'cors',
+    //   url: postURL,
+    //   withCredentials: false,
+    //   params: formData
+    // }).then(({data}) => {
+    //       console.log(data);
+    //   });
+
+    // fetch
+      //   body: JSON.stringify({
+    //     'u': process.env.REACT_APP_MAILCHIMP_U,
+    //     'id': process.env.REACT_APP_MAILCHIMP_ID,
+    //     'MERGE0' : email,
+    //     'MERGE1' :firstName,
+    //     'MERGE2' : lastName,
+    //   headers: {
+    //     'Access-Control-Allow-Origin' : '*',
+    //     'Content-Type': 'application/x-www-form-urlencoded'
+    //   }
+    // })
+
+    fetch(postURL, {
+      method: 'POST',
+      mode: 'no-cors',
+      body: formData
+  
+    })
+    // .then((response) => {
+    //   return response.json()
+    // })
+    .then((data) => {
+      console.log(data);
+    });
+  
+
+
+  }
 
   return (
         <>
-          <form className="mc__form">
+          <form action={postURL} className="mc__form" onSubmit={handleSubmit} >
             <h3 className="mc__title">"Unlock Tomorrow's Tale: Join the Exclusive Newsletter for Our Upcoming Novel!”</h3>
             <div className="mc__field-container">
               <label htmlFor="first-name">First Name:</label>
               <input
                 id="first-name"
+                name="MERGE1"
+
                 onChange={e => setFirstName(e.target.value)} 
                 type="text"
                 value={firstName}
@@ -33,6 +108,7 @@ export default function MailchimpForm() {
               <label htmlFor="last-name">First Name:</label>
               <input
                 id="last-name"
+                name="MERGE2"
                 onChange={e => setLastName(e.target.value)} 
                 type="text"
                 value={lastName}
@@ -42,6 +118,8 @@ export default function MailchimpForm() {
               <label htmlFor="email">Email:</label>
               <input
                 id="email"
+                name="MERGE0"
+
                 onChange={e => setEmail(e.target.value)} 
                 type="email"
                 value={email}
@@ -53,7 +131,7 @@ export default function MailchimpForm() {
                 label="subscribe"
                 type="submit"
                 formvalues={[email, firstName, lastName]}
-              />
+              />Subscribed
           </div>
           </form>
       </>
