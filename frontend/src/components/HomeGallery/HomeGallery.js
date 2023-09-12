ced24adbf7cc8da62c563761303ec911f4685b0e
Alien <lennhypolite@aliens-mbp.myfiosgateway.com>
2023-09-12 02:05:18 -0400

    Change pictures. Change styling of pictures.

--- frontend/src/components/HomeGallery/HomeGallery.js
+++ frontend/src/components/HomeGallery/HomeGallery.js
@@ -16,7 +16,7 @@ export default function HomeGallery() {
       </section>
       <div className="App-header">
         <div className="backgroun-image"></div>
-        <div className="intro-landing"><div>provocative and visceral content that inspires and entertains through art and storytelling</div></div>
+        <div className="intro-landing"><div>Provocative and visceral content that inspires and entertains through art and storytelling</div></div>
 
           <section className="home-gallery">
             {categories.map((cat, i) => (
