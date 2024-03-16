745bb9ae5def987aee13206b26af6fa1af156dca
lenny peny <lennhydesign@gmail.com>
2024-03-16 14:22:11 -0400

    Update Navbar.js

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import { NavLink, useLocation } from "react-router-dom"; // import Link in addition to other Components
 import { useState } from 'react';
 import "./navbar.css";
-import logo from '../../assets/polyverge-logo.jpg';
+import logo from '../../assets/polyverge-logo.png';
 
 // import logo from "../logo.svg";
 
