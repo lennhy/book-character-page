30dbad6780b1f044ec8e9475eecec95ac91ab3ae
lenny peny <lennhydesign@gmail.com>
2024-03-16 14:30:31 -0400

    Update Navbar.js

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -23,8 +23,8 @@ const Navbar = () => {
     <>
       <nav className="navigation">
         <div className="left">
-          {/* <NavLink to="/"> <img src={logo} className="" alt="logo" /></NavLink> */}
-          <NavLink id="logo" to="/">{logo}</NavLink>
+          <NavLink to="/"> <img src={logo} className="" alt="logo"/> </NavLink>
+          {/*  <NavLink id="logo" to="/">{logo}</NavLink> */}
           {addSubNav()}
         </div>
         <div className="right">
